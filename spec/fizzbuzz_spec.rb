281ea557d78940cd70db1a266639d0a8f94777eb
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-04 11:22:40 +0300

    test for reverse method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,23 +1,23 @@
-require './sover' 
+require './sover'
 
-describe Solver do 
-  it 'Fizz for divisible by 3' do 
-    expect( @solver.new.fizzbuzz(3)).to eql 'fizz'
-    expect( @solver.new.fizzbuzz(6)).to eql 'fizz'
+describe Solver do
+  it 'Fizz for divisible by 3' do
+    expect(@solver.new.fizzbuzz(3)).to eql 'fizz'
+    expect(@solver.new.fizzbuzz(6)).to eql 'fizz'
   end
 
-  it 'Fizz for divisible by 5' do 
-    expect( @solver.new.fizzbuzz(5)).to eql 'buzz'
-    expect( @solver.new.fizzbuzz(10)).to eql 'buzz'
+  it 'Fizz for divisible by 5' do
+    expect(@solver.new.fizzbuzz(5)).to eql 'buzz'
+    expect(@solver.new.fizzbuzz(10)).to eql 'buzz'
   end
 
-  it 'Fizz for divisible by 3 & 5' do 
-    expect( @solver.new.fizzbuzz(15)).to eql 'fizzbuzz'
-    expect( @solver.new.fizzbuzz(30)).to eql 'fizzbuzz'
+  it 'Fizz for divisible by 3 & 5' do
+    expect(@solver.new.fizzbuzz(15)).to eql 'fizzbuzz'
+    expect(@solver.new.fizzbuzz(30)).to eql 'fizzbuzz'
   end
 
-  it 'not divisible by 3 or 5' do 
-    expect( @solver.new.fizzbuzz(2)).to eql '2'
-    expect( @solver.new.fizzbuzz(4)).to eql '4'
+  it 'not divisible by 3 or 5' do
+    expect(@solver.new.fizzbuzz(2)).to eql '2'
+    expect(@solver.new.fizzbuzz(4)).to eql '4'
   end
 end
--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -5,17 +5,7 @@ describe Solver do
     @solver = Solver.new
   end
 
-  it 'returns the correct factorial for positive numbers' do
-    expect(@solver.factorial(0)).to eql 1
-    expect(@solver.factorial(1)).to eql 1
-    expect(@solver.factorial(2)).to eql 2
-    expect(@solver.factorial(3)).to eql 6
-    expect(@solver.factorial(4)).to eql 24
-  end
-
-  it 'raises an exception for negative numbers' do
-    expect { @solver.factorial(-1) }.to raise_error('Invalid number')
-    expect { @solver.factorial(-2) }.to raise_error('Invalid number')
-    expect { @solver.factorial(-8) }.to raise_error('Invalid number')
+  it 'reversed word' do
+    expect(solver.reverse('hello')).to eq 'olleh'
   end
 end
